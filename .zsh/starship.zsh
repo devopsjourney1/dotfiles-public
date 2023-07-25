8fc8baa60f1591d95e8b8f5799497089a495bb9b
Brad <Brad@DevOpsJourney>
2023-07-24 18:10:07 -0700

    more tools and fix icon

--- .zsh/starship.zsh
+++ .zsh/starship.zsh
@@ -31,7 +31,7 @@ case $_distro in
     *devuan*)                ICON="";;
     *manjaro*)               ICON="";;
     *rhel*)                  ICON="";;
-    *macos*)                 ICON="";;
+    *macos*)                 ICON="";;
     *)                       ICON="";;
 esac
 
